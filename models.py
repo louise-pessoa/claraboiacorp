962ef524ebd713295584bc933974142c8475d7fd
victormartins <vmtm@cesar.school>
2025-10-01 13:19:37 -0300

    criação do models.py

---
+++ models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.nome)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nome
+
+
+class Tag(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    resumo = models.TextField(max_length=300, blank=True, null=True)
+    conteudo = models.TextField()
+    imagem = models.ImageField(upload_to="noticias/", blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name="noticias")
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="noticias")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="noticias")
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo
