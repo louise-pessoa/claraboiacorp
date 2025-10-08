ba072a7a68a6444f3634f16c3ba66ccc73c04130
LuisGodoy123 <lagi@cesar.school>
2025-10-08 15:26:32 -0300

    criei o super user

--- jcpemobile/admin.py
+++ jcpemobile/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Categoria, Tag, Noticia, Autor
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+	list_display = ('nome', 'slug')
+	search_fields = ('nome',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('nome',)
+	search_fields = ('nome',)
+
+
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+	list_display = ('titulo', 'categoria', 'autor', 'data_publicacao')
+	search_fields = ('titulo', 'resumo', 'conteudo')
+	list_filter = ('categoria', 'autor', 'tags')
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+	list_display = ('nome',)
+	search_fields = ('nome',)
