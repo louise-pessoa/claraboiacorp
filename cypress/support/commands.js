d26474b2a396c42c8f58a130264ac99ccfe0a97f
Louise Pessoa <louisepessoaamds@gmail.com>
2025-11-03 09:54:51 -0300

    corrigindo parte do teste de salvar noticia - testes e2e

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,11 +7,32 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
+
+// Comando customizado para login
+Cypress.Commands.add('login', (email, password) => {
+  // Usar credenciais passadas como parâmetro ou buscar do ambiente
+  const userEmail = email || Cypress.env('TEST_USER_EMAIL')
+  const userPassword = password || Cypress.env('TEST_USER_PASSWORD')
+  
+  // Validar se as credenciais existem
+  if (!userEmail || !userPassword) {
+    throw new Error('Credenciais não configuradas! Verifique o arquivo cypress.env.json')
+  }
+  
+  cy.get('#menuHamburguer').click()
+  cy.get('#linkEntrarMenu').click()
+  cy.get('#login_email').type(userEmail)
+  cy.get('#login_senha').type(userPassword)
+  cy.get('#btnEntrar').click()
+  cy.wait(1000) // Aguardar fechamento do modal
+})
+
+// Comando customizado para acessar notícias salvas
+Cypress.Commands.add('acessarNoticiasSalvas', () => {
+  cy.get('#menuHamburguer').click()
+  cy.contains('Notícias Salvas').click()
+})
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
